89c502fe2173719f6a288224eab710e204a9abe4
Xinggang Li <lishinko@qq.com>
2014-04-01 13:34:33 +0800

    使用整数而不是浮点数
    使用整数14，10来表示路线的距离，而不是1.414，1.0这样的浮点数。性能提升还是比较明显的。
    当然了，这样的精确度就需要重新考虑了。不过征途2就是使用这种整数的办法操作的。县提交再说吧

--- node.h
+++ node.h
@@ -3,23 +3,24 @@
 //是否包含f的值？f = g+h,可以比较简单的计算出来。我之前自己实现的A*就是用这种计算的方式表示f的。现在，看看能不能提高下性能吧
 #include <cstddef>
 typedef int coord_type;
+typedef int dis_type;
 class Node
 {
 public:
     Node();//node是有坐标的，但是没有权重，也就是，每走一步，一定是增加了1（或者1.414）
 
-    void init(const int x, const int y, const bool isBlock){
+    void init(const coord_type x, const coord_type y, const bool isBlock){
         m_x = x; m_y = y; m_isBlock = isBlock;
         /*f = */g = h = 0;
         m_parent = NULL;
     }
-    void setH(const float h){this->h = h;}
-    void setG(const float g){this->g = g; /*updateF();*/}
+    void setH(const dis_type h){this->h = h;}
+    void setG(const dis_type g){this->g = g; /*updateF();*/}
     void setParent(const Node* parent){m_parent = parent;}
 
-    float getF() const {return g+h;}
-    float getG() const {return g;}
-    float getH() const {return h;}
+    dis_type getF() const {return g+h;}
+    dis_type getG() const {return g;}
+    dis_type getH() const {return h;}
 
     //void updateF(){f = g + h;}
     coord_type getX() const {return m_x;}
@@ -30,9 +31,9 @@ public:
     const Node* getParent() const{return m_parent;}
 
 protected:
-    //float f;
-    float g;
-    float h;
+    //dis_type f;
+    dis_type g;
+    dis_type h;
 
     coord_type m_x;
     coord_type m_y;
@@ -47,10 +48,10 @@ public:
         Node::init(x, y, isBlock);
         f = 0;
     }
-    float getF() const {return f;}
-    void setG(const float g){this->g = g; updateF();}
+    dis_type getF() const {return f;}
+    void setG(const dis_type g){this->g = g; updateF();}
 protected:
-    float f;
+    dis_type f;
     void updateF(){f = g + h;}
 };
 
